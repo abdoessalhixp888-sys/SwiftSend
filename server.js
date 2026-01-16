4da4636adc7fc76d64f4b0f753bc41e56660390e
abdoessalhi <abdoessalhixp.888@gmail.com>
2026-01-16 21:35:50 +0100

    Full UI and video support update

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-// نرفع حجم الملف المسموح بنقله إلى 100 ميجابايت مثلاً
+
+// رفع الحد الأقصى إلى 500 ميجابايت
 const io = new Server(server, {
-    maxHttpBufferSize: 1e8 
+    maxHttpBufferSize: 5e8, 
+    pingTimeout: 60000 
 });
 
 app.use(express.static('public'));
@@ -24,22 +26,22 @@ io.on('connection', (socket) => {
     socket.on('join-by-code', (code) => {
         if (activeCodes[code]) {
             socket.join(code);
-            io.to(code).emit('peer-connected', "متصل الآن");
+            io.to(code).emit('peer-connected');
         } else {
             socket.emit('error-message', 'الكود غير صحيح');
         }
     });
 
-    // الجزء الجديد: استقبال الملف وتمريره
     socket.on('send-file', (data) => {
-        // نرسل الملف لكل الموجودين في نفس الغرفة (الكود)
         socket.to(data.code).emit('receive-file', {
             fileData: data.fileData,
-            fileName: data.fileName
+            fileName: data.fileName,
+            fileType: data.fileType
         });
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Server is running...');
 });
